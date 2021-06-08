6929084caa3099cda07eae99fe990b79d3d8940b
Divya950 <77933276+Divya950@users.noreply.github.com>
2021-06-09 00:49:16 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,169 @@
+const keys = document.querySelectorAll(".key")
+keys.forEach(key =>{
+    key.addEventListener("click", () => playNote(key))
+
+})
+function playNote(key){
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime=0;
+    noteAudio.play()
+}
+
+
+
+
+
+
+function blue(){
+    document.getElementById("mydiv").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv1").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv2").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv3").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv4").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv5").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv6").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv7").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv8").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv9").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv10").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv11").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv12").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv13").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv14").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv15").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv16").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv17").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv18").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv19").style.backgroundColor="rgb(228, 129, 129)"
+    document.getElementById("mydiv20").style.backgroundColor="rgb(228, 129, 129)"
+}
+
+
+function violet(){
+    document.getElementById("mydiv").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv1").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv2").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv3").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv4").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv5").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv6").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv7").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv8").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv9").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv10").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv11").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv12").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv13").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv14").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv15").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv16").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv17").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv18").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv19").style.backgroundColor=" rgb(250, 165, 68)"
+    document.getElementById("mydiv20").style.backgroundColor=" rgb(250, 165, 68)"
+}
+
+function indigo(){
+ document.getElementById("mydiv").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv1").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv2").style.backgroundColor=" rgba(220, 236, 76, 0.945)"   
+ document.getElementById("mydiv3").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv4").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv5").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv6").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv7").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv8").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv9").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv10").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv11").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv12").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv13").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv14").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv15").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv16").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv17").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv18").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv19").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+ document.getElementById("mydiv20").style.backgroundColor=" rgba(220, 236, 76, 0.945)"
+}
+
+
+function orange(){
+ document.getElementById("mydiv").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv1").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv2").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv3").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv4").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv5").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv6").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv7").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv8").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv9").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv10").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv11").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv12").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv13").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv14").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv15").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv16").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv17").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv18").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv19").style.backgroundColor=" rgb(170, 236, 170)"
+ document.getElementById("mydiv20").style.backgroundColor=" rgb(170, 236, 170)"
+
+}
+function grey(){
+ document.getElementById("mydiv").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv1").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv2").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv3").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv4").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv5").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv6").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv7").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv8").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv9").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv10").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv11").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv12").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv13").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv14").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv15").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv16").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv17").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv18").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv19").style.backgroundColor=" rgb(172, 166, 226)"
+ document.getElementById("mydiv20").style.backgroundColor=" rgb(172, 166, 226)"
+
+
+}
+
+function brown(){
+ document.getElementById("mydiv").style.backgroundColor=" rgb(164, 70, 226)"
+ document.getElementById("mydiv1").style.backgroundColor=" rgb(164, 70, 226)"
+ document.getElementById("mydiv2").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv3").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv4").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv5").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv6").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv7").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv8").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv9").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv10").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv11").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv12").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv13").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv14").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv15").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv16").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv17").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv18").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv19").style.backgroundColor="rgb(164, 70, 226)"
+ document.getElementById("mydiv20").style.backgroundColor="rgb(164, 70, 226)"
+
+}
+
+
+
+
+
